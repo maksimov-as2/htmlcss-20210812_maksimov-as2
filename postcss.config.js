363b0867271bf116aa5e6f6319bd68d3ce13213e
Максимов Андрей Сергеевич <Maksimov-AS2@corp.groupmegapolis.ru>
2021-09-09 14:07:06 +0300

    postcss features implemented

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    syntax: 'postcss-scss',
+    map: { inline: false },
+    plugins: [
+        require('precss')({
+            lookup: { disable: true },
+            properties: {
+                // Чтобы работали CSS custom properties, а не заменялись на статическое значение.
+                disable: true,
+                preserve: true,
+            },
+        }),
+        require('postcss-import'),
+        require('postcss-nested'),
+        require('postcss-advanced-variables'),
+        require('postcss-strip-inline-comments'),
+        require('autoprefixer'),
+        require('cssnano'),
+    ],
+}
