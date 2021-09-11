a892ddc1293b5613c2653d12ea5a635e3cf202b4
Андрей Максимов <maksimov-as2@waterfall-pro.ru>
2021-09-11 15:12:02 +0300

    mixins implemented

--- postcss.config.js
+++ postcss.config.js
@@ -2,19 +2,12 @@ module.exports = {
     syntax: 'postcss-scss',
     map: { inline: false },
     plugins: [
-        require('precss')({
-            lookup: { disable: true },
-            properties: {
-                // Чтобы работали CSS custom properties, а не заменялись на статическое значение.
-                disable: true,
-                preserve: true,
-            },
-        }),
         require('postcss-import'),
         require('postcss-nested'),
+        require('postcss-mixins'),
         require('postcss-advanced-variables'),
-        require('postcss-strip-inline-comments'),
         require('autoprefixer'),
+        require('postcss-strip-inline-comments'),
         require('cssnano'),
     ],
 };
